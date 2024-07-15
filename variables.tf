f5dedbe403c8f39a745257572048e46a08d1a7b1
anilkumarnagaraj <anilkumar@in.ibm.com>
2024-07-15 07:33:28 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,8 @@
-/*variable "ibmcloud_api_key" {
+variable "ibmcloud_api_key" {
   description = "IBM Cloud Platform API Key"
 }
 
+/*
 variable "resource_group_name" {
   description = "Where to create resources"
   default = "Default"
